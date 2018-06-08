b907d47500e755ef2bbbacda44903da57815077a
Durgamahesh <Durgamahesh@LAPTOP-9NH1DG4L>
2018-06-08 07:50:36 +0530

    Test append java file is created

---
+++ Learn/src/com/varahi/welcome/TestAppend.java
@@ -0,0 +1,13 @@
+package com.varahi.welcome;
+
+public class TestAppend {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer("I am Learning");
+		sb.append(" Git Hub");
+		System.out.println(sb);
+
+	}
+
+}
